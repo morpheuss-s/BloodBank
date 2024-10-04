012686c6ff63f1452e9f45ac846623c897cef329
Gage Gjorgjeski <147656966+GageMG@users.noreply.github.com>
2024-10-04 01:24:12 -0400

    Maybe this would work?
    Let me know if there has to be changes or certain things need to be added. I just did what I thought would be needed.

---
+++ Donation.java
@@ -0,0 +1,40 @@
+import java.time.LocalDate;
+
+public class Donation {
+    private String donorName;
+    private String bloodType;
+    private int Amount;
+    private LocalDate donoDate;
+
+public Donation(String donorName, String bloodType, int Amount, LocalDate donoDate) {
+    this.donorName = donorName;
+    this.bloodType = bloodType;
+    this.Amount = Amount;
+    this.donoDate = donoDate;
+    }
+
+    public String getDonorName() {
+        return donorName;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public int getAmount() {
+        return Amount;
+    }
+
+    public LocalDate getDonoDate() {
+        return donoDate;
+    }
+
+    public String allInfo() {
+        return "Blood Donated{" +
+                "DonorName: " + getDonorName() + '\'' +
+                ", bloodType: " + getBloodType() + '\'' +
+                ", Amount: " + getAmount() + '\'' +
+                ", DonationDate: " + getDonoDate() +
+                '}'
+    }
+}
